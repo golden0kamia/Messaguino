025f6023dbdf80791363b8ff21e61c10bf275302
Zérock <golden0kamia@gmail.com>
2018-01-23 23:22:30 +0100

    update

--- MSGO_Intertechno/MSGO_Intertechno.ino
+++ MSGO_Intertechno/MSGO_Intertechno.ino
@@ -1,4 +1,6 @@
 #include <LiquidCrystal.h>
+#include <NewRemoteReceiver.h>
+#include <NewRemoteTransmitter.h>
 
 
 //---------- Global variable ----------// DO NOT CHANGE
@@ -17,10 +19,22 @@ const int RX  = 8;	//
 const int TX  = 13;	//
 
 //---------- Other global variable ----------//
+int i = 0;
+unsigned long int adresse = 13709258;
+int period = 253;
+char unit = 0;
+char repeat = 4;
+char state[16];
+String menuSelec[] = {"1:Change adresse",
+					"2:Change period",
+					"3:Change repeat"};
+NewRemoteTransmitter transmitter(adresse, TX, period, repeat);
 
 
 //---------- Functions ----------//
+void menu();
 char key();
+
 
 //---------- Setup function ----------//
 void setup()
@@ -34,69 +48,97 @@ void setup()
 	digitalWrite(MX3, LOW);
 	digitalWrite(RET, HIGH);	//Active RET
 	lcd.begin(16,2);			//Initialise le LCD
+ Serial.begin(9600);
+ for(i = 0; i < 16; i++)
+ {
+  state[i] = 0;
+ }
+	
 	lcd.setCursor(3, 0);
-	lcd.print("Hello World!");
+	lcd.print("Intertechno");
+	lcd.setCursor(6, 1);
+	lcd.print("Remote");
 	delay(2000);
+	lcd.clear();
 }
 
 //---------- Main loop function ----------//
 void loop() 
 {
-	lcd.print(key());
+	lcd.print("Select Unit");
+	lcd.setCursor(12, 1);
+	lcd.print("Menu");
+  unit = key();
+  Serial.println(unit);
+	if(unit == '*')
+	{
+		menu();
+	}
+	else if(unit == '#');
+	{}
+  state[unit] = !state[unit];
+	transmitter.sendUnit(unit, state[unit]);
+}
+
+//---------- Main menu of the systeme ----------//
+void menu()
+{
+	//Menu here
 }
 
 //---------- Get key pressed function ----------//
 char key()
 {
-	char out = 0;
+	char out = -1;
 	do
 	{
+  Serial.println("wait key");
 		digitalWrite(MX1, HIGH);
 			if(digitalRead(MY1))
 			{
-				out = '1';
+				out = 1;
 			}else if(digitalRead(MY2))
 			{
-				out = '4'
+				out = 4;
 			}else if(digitalRead(MY3))
 			{
-				out = '7';
+				out = 7;
 			}else if(digitalRead(MY4))
 			{
-				out = '#';
+				out = '*';
 			}
 		digitalWrite(MX1, LOW);
 		digitalWrite(MX2, HIGH);
 			if(digitalRead(MY1))
 			{
-				out = '2';
+				out = 2;
 			}else if(digitalRead(MY2))
 			{
-				out = '5'
+				out = 5;
 			}else if(digitalRead(MY3))
 			{
-				out = '8';
+				out = 8;
 			}else if(digitalRead(MY4))
 			{
-				out = '0';
+				out = 0;
 			}
 		digitalWrite(MX2, LOW);
 		digitalWrite(MX3, HIGH);
 			if(digitalRead(MY1))
 			{
-				out = '3';
+				out = 3;
 			}else if(digitalRead(MY2))
 			{
-				out = '6'
+				out = 6;
 			}else if(digitalRead(MY3))
 			{
-				out = '9';
+				out = 9;
 			}else if(digitalRead(MY4))
 			{
-				out = '*';
+				out = '#';
 			}
 		digitalWrite(MX3, LOW);
-	}while(out == 0);
-	
+	}while(out == -1);
+	delay(500);
 	return out;
 }
--- Messaguino_Base/Messaguino_Base.ino
+++ Messaguino_Base/Messaguino_Base.ino
@@ -1,6 +1,4 @@
 #include <LiquidCrystal.h>
-#include <NewRemoteReceiver.h>
-#include <NewRemoteTransmitter.h>
 
 
 //---------- Global variable ----------// DO NOT CHANGE
@@ -19,20 +17,10 @@ const int RX  = 8;	//
 const int TX  = 13;	//
 
 //---------- Other global variable ----------//
-unsigned long int adresse = 13709258;
-int period = 253;
-char unit = 0;
-char repeat = 4;
-char state[16] = 0;
-char menuSelec[] = {"1:Change adresse",
-					"2:Change period",
-					"3:Change repeat"};
 
 
 //---------- Functions ----------//
-void menu();
 char key();
-
 
 //---------- Setup function ----------//
 void setup()
@@ -46,36 +34,15 @@ void setup()
 	digitalWrite(MX3, LOW);
 	digitalWrite(RET, HIGH);	//Active RET
 	lcd.begin(16,2);			//Initialise le LCD
-	NewRemoteTransmitter transmitter(adresse, TX, period, repeat);
-	
 	lcd.setCursor(3, 0);
-	lcd.print("Intertechno");
-	lcd.setCursor(6, 1);
-	lcd.print("Remote");
+	lcd.print("Hello World!");
 	delay(2000);
-	lcd.clear();
 }
 
 //---------- Main loop function ----------//
 void loop() 
 {
-	lcd.print("Select Unit");
-	lcd.setCursor(13, 1);
-	lcd.print("Menu");
-	unit = key()-1;
-	if(unit == '*')
-	{
-		menu();
-	}
-	else if(unit == '#');
-	{}
-	transmitter.sendUnit(unit, !state[unit])
-}
-
-//---------- Main menu of the systeme ----------//
-void menu()
-{
-	//Menu here
+	lcd.print(key());
 }
 
 //---------- Get key pressed function ----------//
@@ -87,13 +54,13 @@ char key()
 		digitalWrite(MX1, HIGH);
 			if(digitalRead(MY1))
 			{
-				out = 1;
+				out = '1';
 			}else if(digitalRead(MY2))
 			{
-				out = 4
+				out = '4';
 			}else if(digitalRead(MY3))
 			{
-				out = 7;
+				out = '7';
 			}else if(digitalRead(MY4))
 			{
 				out = '#';
@@ -102,28 +69,28 @@ char key()
 		digitalWrite(MX2, HIGH);
 			if(digitalRead(MY1))
 			{
-				out = 2;
+				out = '2';
 			}else if(digitalRead(MY2))
 			{
-				out = 5
+				out = '5'
 			}else if(digitalRead(MY3))
 			{
-				out = 8;
+				out = '8';
 			}else if(digitalRead(MY4))
 			{
-				out = 0;
+				out = '0';
 			}
 		digitalWrite(MX2, LOW);
 		digitalWrite(MX3, HIGH);
 			if(digitalRead(MY1))
 			{
-				out = 3;
+				out = '3';
 			}else if(digitalRead(MY2))
 			{
-				out = 6
+				out = '6'
 			}else if(digitalRead(MY3))
 			{
-				out = 9;
+				out = '9';
 			}else if(digitalRead(MY4))
 			{
 				out = '*';
